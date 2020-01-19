70454ebd22679a8d817b0d63dbe7419b4a7ecfea
Víctor Peralta <victorperalta.correo@gmail.com>
2020-01-19 15:12:22 +0100

    controlador adaptado a nuevas rutas

--- controller/principal.php
+++ controller/principal.php
@@ -1,57 +1,76 @@
 <?php 
 require_once 'controller/entorno.php';
+require_once 'models/productos.php';
+require_once 'models/proveedores.php';
 
 function renderizarPrincipal(){
     $entorno = Entorno::getInstancia();
     $variables = [];
 
-    echo $entorno->renderizar("principal.html.twig",$variables);
+    echo $entorno->renderizar("principal.html",$variables);
 }
 
 function renderizarGestionAlmacenes(){
     $entorno = Entorno::getInstancia();
     $variables = [];
     
-    echo $entorno->renderizar("gestion_almacen.html",$variables);
+    echo $entorno->renderizar("gestion_almacen.html.twig",$variables);
 }
 
 function renderizarGestionProductos(){
     $entorno = Entorno::getInstancia();
-    $productos = array();
-
-    $productos[0] = [
-        "id" => 1,
-        "nombre" => "tablero de madera de pino",
-        "cantidad" => 7
-    ];
-
-    $productos[1] = [
-        "id" => 2,
-        "nombre" => "tablero de madera de roble",
-        "cantidad" => 14
-    ];
-
-    $productos[2] = [
-        "id" => 3,
-        "nombre" => "placa de aluminio",
-        "cantidad" => 23
-    ];
-
-    $productos[3] = [
-        "id" => 4,
-        "nombre" => "plancha de PVC",
-        "cantidad" => 165
-    ];
-
-    $productos[4] = [
-        "id" => 5,
-        "nombre" => "placa de acero",
-        "cantidad" => 65
-    ];
+    $productos = obtenerProductos();
 
     $variables = [
         "productos" => $productos,
     ];
 
     echo $entorno->renderizar("gestion_productos.html.twig",$variables);
+}
+
+function renderizarGestionProductosAlmacen(){
+    $entorno = Entorno::getInstancia();
+    $productos = obtenerProductos();
+
+    $variables = [
+        "productos" => $productos,
+    ];
+
+    echo $entorno->renderizar("gestion_productos_almacen.html",$variables);
+}
+
+
+function renderizarGestionPedidos(){
+    $entorno = Entorno::getInstancia();
+    $variables = [];
+    
+    echo $entorno->renderizar("gestion_pedidos.html.twig",$variables);
+}
+
+
+function renderizarProveedoresPedidos(){
+    $entorno = Entorno::getInstancia();
+    $variables = [];
+    
+    echo $entorno->renderizar("proveedores_pedidos.html",$variables);
+}
+
+function renderizarProduccionAlmacenaje(){
+    $entorno = Entorno::getInstancia();
+    $variables = [];
+    
+    echo $entorno->renderizar("produccion_almacenaje.html",$variables);
+}
+
+function renderizarHacerPedido(){
+    $entorno = Entorno::getInstancia();
+    $productos = obtenerProductos();
+    $proveedores = obtenerProveedores();
+
+    $variables = [
+        "productos" => $productos,
+        "proveedores" => $proveedores
+    ];
+    
+    echo $entorno->renderizar("hacerpedido.html.twig",$variables);
 }
